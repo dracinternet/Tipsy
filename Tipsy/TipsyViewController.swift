58f8a4477d6da197102a09c952f77f82f12b2771
Eduardo Lozano <eduardo.lozano@dracinternet.com>
2016-08-20 15:41:51 +0200

    Added the assets and renamed the main view controller.

---
+++ Tipsy/TipsyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Eduardo Lozano Gálvez on 20/8/16.
+//  Copyright © 2016 DracInternet. All rights reserved.
+//
+
+import UIKit
+
+class TipsyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
